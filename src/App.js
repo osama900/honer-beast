53bc8a08b6a8924e79617ef355c9a18ec07e94ad
osama900 <osamaos2233@gmail.com>
2021-08-29 17:10:29 +0300

    first components created

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
+import Main from "./components/Main";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return <p>@ OSAMA OMAR</p>;
+  }
+}
+export default Footer;
